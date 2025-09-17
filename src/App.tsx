2c07ee15f4a5852e6352a8bbc7c8dfd6ff2cef5d
DarioDeLaFuente <livet.til.dario@gmail.com>
2025-09-17 07:06:26 +0200

    history in the making

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { ImageGallery360 } from '@/components/ui/ImageGallery360'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import './App.css'
 
 function App() {
@@ -18,20 +20,26 @@ function App() {
           imageUrl="/images/360/bakgrunn.avif"
           autoRotate={true}
         />
+        <div className="relative z-2 w-[55svh]">
+        <div className="relative z-2 h-[15svh]  w-[55svh] flex justify-center  text-white pt-4"><Label htmlFor="email" className="text-xl">Meld deg på forhåndssalget</Label></div>
+        <div className="relative z-2 bg-white/80 text-black p-8 backdrop-blur-lg pointer-events-auto">
+      <div className="grid w-full max-w-sm items-center gap-3 flex justify-center">
+      <Input type="email" id="email" placeholder="Din epost" />
       
-      <div className="relative z-3">
       <Sheet>
-        <SheetTrigger className="text-white bg-black hover:bg-gray-800 p-4">Meld deg på forhåndssalget</SheetTrigger>
+        <SheetTrigger className="text-white bg-black hover:bg-gray-800 p-5 w-[40svh] flex justify-center">Meld deg på</SheetTrigger>
         <SheetContent>
           <SheetHeader>
-            <SheetTitle>HEI DET ER MEG</SheetTitle>
+            <SheetTitle>HEI</SheetTitle>
             <SheetDescription>
-            RING MEG MIKE @ KARPEWORLD.
+            RING MEG .
             </SheetDescription>
           </SheetHeader>
         </SheetContent>
       </Sheet>
       </div>
+    </div>
+    </div>
     </div>
   )
 }
