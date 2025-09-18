c37374773ae7c031cc49bd4d87ab96e2dfee09f5
DarioDeLaFuente <livet.til.dario@gmail.com>
2025-09-18 20:11:33 +0200

    history in the making

--- src/App.tsx
+++ src/App.tsx
@@ -35,13 +35,13 @@ function App() {
         
         {showSignUpForm && (
           <div className="relative z-10 w-[55svh]">
-          <div className="relative h-[15svh]  w-[55svh] flex justify-center  text-white pt-4"><Label htmlFor="email" className="text-xl">Meld deg på forhåndssalget</Label></div>
-          <div className="relative bg-white/80 text-black p-8 backdrop-blur-lg pointer-events-auto">
+          <div className="relative h-[15svh]  w-[55svh] flex justify-center  text-white pt-4"><Label htmlFor="email" className="uppercase col-span-3 text-2xl md:text-4xl">utsolgt!</Label></div>
+          <div className="relative bg-white/20 text-white p-2 border rounded-xl border-white md:p-4 backdrop-blur-lg pointer-events-auto pointer-events-auto">
         <div className="grid w-full max-w-sm items-center gap-3 flex justify-center">
-        <Input type="email" id="email" placeholder="Din epost" />
+        <Input className="w-full px-1 py-2 rounded-none border-t-0 border-r-0 border-l-0 border-b-1 border-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent disabled:bg-gray-100 disabled:cursor-not-allowed" type="email" id="email" placeholder="Din epost" />
         
         <Sheet>
-          <SheetTrigger className="text-white bg-black hover:bg-gray-800 p-5 w-[40svh] flex justify-center">Meld deg på</SheetTrigger>
+          <SheetTrigger className="w-full bg-black text-white py-2 px-4  hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition-colors disabled:bg-gray-400 disabled:cursor-not-allowed uppercase cursor-pointer">KARPE WORLD NYHETSBREV</SheetTrigger>
           <SheetContent>
             <SheetHeader>
               <SheetTitle>HEI</SheetTitle>
@@ -50,6 +50,7 @@ function App() {
               </SheetDescription>
             </SheetHeader>
           </SheetContent>
+          <div className="w-full text-[8px] md:text-xs mt-2 text-left text-white-500">Ved å melde deg på godtar du <a className="underline" href="/personvern">personvernerklæringen</a></div>
         </Sheet>
         </div>
       </div>
