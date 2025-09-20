83b0172b1c20e36de973a4f7feb3c123a7ebcf7a
DarioDeLaFuente <livet.til.dario@gmail.com>
2025-09-20 16:18:57 +0200

    history in the making

--- src/App.tsx
+++ src/App.tsx
@@ -40,9 +40,9 @@ function App() {
         
         {showSignUpForm && deviceMotionAllowed && ( // Render only if both conditions are true
           <div className="relative z-10 w-[55svh]">
-          <div className="relative h-[15svh]  w-[55svh] flex justify-center  text-white pt-4"><Label htmlFor="email" className="uppercase col-span-3 text-2xl md:text-3xl">Mine damer og herrer!</Label></div>
+          <div className="relative h-[10svh] w-[55svh] flex justify-center  text-white pt-4"><Label htmlFor="email" className="uppercase col-span-3 text-2xl md:text-3xl">Mine damer og herrer!</Label></div>
           <div className="relative bg-white/20 text-white p-2 border rounded-xl border-white md:p-4 backdrop-blur-lg pointer-events-auto pointer-events-auto">
-        <div className="grid w-full max-w-sm items-center gap-3 flex justify-center">
+        <div className="grid w-full max-w-sm items-center gap-1 flex justify-center">
       
         
         <Sheet>
